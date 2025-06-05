bf75da770c8d41dabcdc03e88732360a14e9e2a7
Chris Najman <christopher.najman@gmail.com>
2025-06-05 15:17:32 +0100

    Changed LOCAL_STORAGE_PREFIX to unique name in theme.js

--- js-modules/theme.js
+++ js-modules/theme.js
@@ -1,5 +1,5 @@
 export default function themeSwitcher() {
-  const LOCAL_STORAGE_PREFIX = "SIMPLE_TEMPLATE"
+  const LOCAL_STORAGE_PREFIX = "CSS-FORM-STYLING"
   const THEME_KEY = `${LOCAL_STORAGE_PREFIX}-theme`
 
   const themeToggler = document.querySelector(".theme-toggler")
